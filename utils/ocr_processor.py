442c6579cd40b872b90cbc11d4e0869dc6be8923
Deepika Kushwaha <deepikamau9794@gmail.com>
2026-07-19 19:16:44 +0530

    Add OCR support for deployment

--- utils/ocr_processor.py
+++ utils/ocr_processor.py
@@ -1,13 +1,8 @@
-import io
+import io                      
 
 import pytesseract
 
 from PIL import Image
-
-
-pytesseract.pytesseract.tesseract_cmd = (
-    r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-)
 
 
 def extract_text_from_image(image_bytes):
